b5f61189f328a3028308ddca539480269505574f
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2023-11-12 23:31:05 -0800

    publish

---
+++ src/api/zip/controllers/zip.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * zip controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::zip.zip');
---
+++ src/api/zip/routes/zip.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * zip router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::zip.zip');
